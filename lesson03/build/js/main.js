c46ba3a778ad800613cfaf70fa2caefca9000679
Colton Hyland <colthyland@gmail.com>
2023-04-08 16:18:14 -0700

    Start lesson 3

---
+++ lesson03/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let myName = "Colton";
+let meaningOfLife;
+let isLoading;
+let album;
+//let album: string | number;
+myName = "Tyrell";
+meaningOfLife = 42;
+isLoading = true;
+album = "Van Halen";
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson03/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = "Colton";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+//let album: string | number;
+
+myName = "Tyrell";
+meaningOfLife = 42;
+isLoading = true;
+album = "Van Halen";
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g
