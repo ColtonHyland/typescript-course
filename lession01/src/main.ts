5a81e228a5ab39bc0021d1e0daf3cd7bd988c3a0
Colton Hyland <colthyland@gmail.com>
2023-04-08 15:39:21 -0700

    Lesson 1 covering typescript config and compiler

--- lession01/src/main.ts
+++ lession01/src/main.ts
@@ -1,3 +1,11 @@
-let username = 'Tyrell';
-
+let username = "Tyrell";
 console.log(username);
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b)
+
+console.log(c * b)
+
+//noEmitOnError in tsconfig stops compiler from creating js.
