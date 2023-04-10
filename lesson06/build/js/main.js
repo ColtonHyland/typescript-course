09433eb25cad2e687b06c0a0b6a4892cd65990c9
Colton Hyland <colthyland@gmail.com>
2023-04-10 13:44:03 -0700

    Lesson06 covers types uses in classes, subclasses, constructors and interfaces with classes.

--- lesson06/build/js/main.js
+++ lesson06/build/js/main.js
@@ -1,25 +1,83 @@
 "use strict";
-// convert to more/less specific
-let a = "hello";
-let b = a; // less specific type
-let c = a; // more specific
-let d = "world";
-let e = "world"; // syntax won't work in React
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b;
-};
-//tells typescript to ignore type error
-let myVal = addOrConcat(2, 2, 'concat');
-// Be careful, we told typescript there is no problem - but a string
-// is returned.
-let nextVal = addOrConcat(2, 2, 'concat');
-//10 as string;
-10; // forced Casting
-// The Document Object Model (DOM)
-const img = document.querySelector('img'); // non-null assertion
-const myImg = document.getElementById('#img');
-const nextImg = document.getElementById('#img'); // does not work in react
-img.src;
-myImg.src;
+class Coder {
+    constructor(name, music, age, lang = "Typescript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello, I'm ${this.age}`;
+    }
+}
+const Colton = new Coder("Colton", "Rap", 27);
+console.log(Colton.getAge());
+// console.log(Colton.age)
+// console.log(Colton.lang)
+class WebDev extends Coder {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Tyrell = new WebDev("Lenovo", "Tyrell", "Metal", 22);
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}.`;
+    }
+}
+const Alex = new Guitarist("Alex", "guitar");
+console.log(Alex.play("strums"));
+///////////////////////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps('John');
+const Steve = new Peeps('Steve');
+const Amy = new Peeps('Amy');
+console.log(John.id, Peeps.count);
+console.log(Steve.id, Peeps.count);
+console.log(Amy.id, Peeps.count);
+////////////////////////////////////////////////////////////////
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error('Param is not an array of strings!');
+    }
+}
+const myBands = new Bands();
+myBands.data = ['Neil Young', 'Led Zeppelin'];
+console.log(myBands.data);
+myBands.data = [...myBands.data, 'ZZ Top'];
+console.log(myBands.data);
+//myBands.data = ['Van Halen', 5150]
