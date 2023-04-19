46e703a1bd8448fb3e83cdd6bc9385c873b5267d
Colton Hyland <colthyland@gmail.com>
2023-04-18 18:58:03 -0700

    Lesson13 covers Hooks like useMemo, useRef, useEffect

--- lesson13/typescript-react-app/src/App.tsx
+++ lesson13/typescript-react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback, MouseEvent, useMemo, useRef, KeyboardEvent } from "react";
 import "./App.css";
 
 interface User {
@@ -6,9 +6,24 @@ interface User {
   username: string;
 }
 
+type fibFunc = (n: number) => number
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n
+  return (fib(n - 1) + fib(n - 2))
+}
+
+const myNum: number = 25
+
 function App() {
   const [count, setCount] = useState<number>(0);
   const [users, setUsers] = useState<User[] | null>(null);
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  console.log(inputRef?.current)
+  console.log(inputRef?.current?.value)
+
 
   useEffect(() => {
     console.log('mounting')
@@ -16,12 +31,16 @@ function App() {
     return () => console.log("unmounting")
   }, [users])
 
-  con
+  const addTwo = useCallback((e: MouseEvent<HTMLButtonElement> | KeyboardEvent<HTMLButtonElement>): void => setCount(prev => prev + 2), [])
+
+  const result = useMemo<number>(() => fib(myNum), [myNum])
 
   return (
     <div className="App">
       <h1>{count}</h1>
-      <button onClick={() => setCount(prev => prev + 1)}>Add</button>  
+      <button onClick={addTwo}>Add</button> 
+      <h2>{result}</h2>
+      <input ref={inputRef} type="text" />
     </div>
   );
 }
